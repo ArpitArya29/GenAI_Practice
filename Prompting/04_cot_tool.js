ab12f0f55ccf9fbccb92469928b696c61073f724
ArpitArya29 <arpitaryan48@gmail.com>
2026-07-06 23:36:14 +0530

    Passing tools to the models

--- Prompting/04_cot_tool.js
+++ Prompting/04_cot_tool.js
@@ -1,5 +1,6 @@
 import { OpenAI } from "openai"
 import  dotenv from "dotenv"
+import axios from 'axios'
 
 dotenv.config({
     path: "../.env"
@@ -14,10 +15,20 @@ const client = new OpenAI({
     baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
 })
 
+async function getWeatherData(cityName) {
+    const url = `https://wttr.in/${cityName.toLowerCase()}?format=%C+%t`
+    const response = await axios.get(url, {responseType: 'text'})
+    
+    return JSON.stringify({
+        cityName,
+        weatherInfo: response.data
+    })
+}
+
 const SYSTEM_PROMPT = `
     You are an expert AI engineer. You have to analyze the user's input carefully and you need to break down the problem into multiple subproblems before coming on to the final result. Always breakdown the users intention and how to solve that problem and then step by step solve it.
 
-    We are goint to follow the pipeline of "INITIAL", "THINK", "ANALYSE" and "OUTPUT" pipeline.
+    We are goint to follow the pipeline of "INITIAL", "THINK", "TOOL_REQUEST", "ANALYSE" and "OUTPUT" pipeline.
 
     The popelines:
     - "INITIAL" when user gives an input, we will have an initial touught process on what this user is trying to do
@@ -25,7 +36,11 @@ const SYSTEM_PROMPT = `
     - "ANALYSE" this is where we analyse the solution and also verify if the output is correct
     - "THINK" we can go to think mode where we now see if any sub-problem remains and think
     - "ANALYSE" again analyse the problem and get into the solution
+    - "TOOL_REQUEST": use this for calling or requesting a tool. The format of output would be {"step": "TOOL_REQUEST", functionName: "getWeatherData", "input": "Goa"}
     - "OUTPUT" this is where we can end and give the final output to the user
+
+    Available Tools:
+    - "getWeatherData": getWeatherData(cityName: string): Returns the realtime weather information of city
 
     Rules:
     - Always output one step at a time and wait for other step before proceeding
@@ -33,7 +48,7 @@ const SYSTEM_PROMPT = `
     - Always follow JSON output format strictly
     - Return only a valid JSON object. Do not wrap it in Markdown code fences. Do not include any explanation before or after the JSON.
 
-    Examples:
+    Example:
     - "USER": What is 2 + 2 - 5 * 10 / 3?
     OUTPUT:
     - "INITIAL": "The user wants me to solve a maths equation"
@@ -46,8 +61,19 @@ const SYSTEM_PROMPT = `
     - "THINK": "After the final substraction, the ans remains as -12.66667"
     - "OUTPUT": "The final output is -12.66667"
 
+    Example:
+    - "USER": what is weather of Goa?
+    OUTPUT:
+    - "INITIAL": "The user wants me to fetch weather information of Goa"
+    - "THINK": "From the tools, I can see we have a tool named getWeatherData which can be called"
+    - "ANALYSE": "We are going right, we can call getWeatherData with "GOA" as input"
+    - "TOOL_REQUEST": {"functionName": "getWeatherData", "input": "goa"}
+    - "TOOL_OUTPUT": "The weather of Goa is something like 30 degree C"
+    - "THINK": "We got the weather info"
+    - "OUTPUT": "The weather of Goa is sunny with some 30 degree C. Its gonna be Hotttt"
+
     Output Formats:
-    { "step": "INITIAL" | "THINK" | "ANALYSE" | "OUTPUT", "text": "<The Actual Text>" }
+    { "step": "INITIAL" | "THINK" | "ANALYSE" | "OUTPUT", "text": "<The Actual Text>", "functionName": <NAME OF FUNCTION>", "input": "INPUT PARAMS of function" }
 `;
 
 const MESSAGES_DB = [
@@ -73,18 +99,32 @@ async function main(prompt = '') {
         const parsedRes = JSON.parse(rawResult);
 
         MESSAGES_DB.push({ role:'assistant', content: rawResult })
-
-        // console.log(`🤖(${parsedRes.step}) : ${parsedRes.text}`);
-
-        // if(parsedRes.step.toLowerCase() === "think") {
-        //     // TODO: We can call another API to validate the current result
-
-        //     // We can validate the output of the chatGPT from Claude
-        // }
         
         if(parsedRes.step.toLowerCase() === "output") {
             console.log(parsedRes.text);
             break;
+        }
+
+        if(parsedRes.step.toUpperCase() === "TOOL_REQUEST") {
+            const { functionName, input } = parsedRes;
+
+            switch(functionName) {
+                case 'getWeatherData': 
+                    {
+                        const toolRes = await getWeatherData(input);
+                        console.log(`⛏️=>(${functionName}): ${input} ${toolRes}`);
+                        
+                        MESSAGES_DB.push({
+                            role: 'user',
+                            content: JSON.stringify({
+                                step: 'TOOL_OUTPUT',
+                                output: toolRes
+                            })
+                        })
+                        continue;
+                    }
+                    break;
+            }
         }
 
         MESSAGES_DB.push({
@@ -95,4 +135,4 @@ async function main(prompt = '') {
 
 }
 
-main("What is Generative AI, give the brief response in about 20 - 30 words?");
+main("What is the weather of Hazaribagh, Delhi and Bangalore?");
